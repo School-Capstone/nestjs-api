6a82e19f578010ad68dfe8e4c0c9f78e82a68a43
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-03-13 16:08:32 +0200

    🚓 ft: add guards & create a custom GetUser guard 💂

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe() {
-    return 'me';
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch('me')
+  updateUser() {
+    return 'update me';
   }
 }
