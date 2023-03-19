74e41dc69637187e7239e46ebe45e4d3f2a98c9c
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2023-03-19 12:24:53 +0200

    📜 ft: add base structure for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('My Blog')
+    .setDescription('A Simple Blog API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
